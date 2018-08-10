da96999586b7d7d68e06d2218ca68e71ea77d82a
Piotr Tecza <Piotr.Tecza@clear.bank>
2018-08-10 13:41:35 +0100

    graham scan

--- Test/Code.fs
+++ Test/Code.fs
@@ -1,11 +1,12 @@
 ﻿module Code
+open System.Collections.Generic
 
 type Point = { X: int; Y: int; }
 
 let ccw p1 p2 p3 = (p2.X - p1.X)*(p3.Y - p1.Y) - (p2.Y - p1.Y)*(p3.X - p1.X)
 
 let findMin points =
-    let mutable min = points |> Seq.head
+    let mutable min = points |> List.head
     for p in points do
         if(p.Y < min.Y || (p.Y = min.Y && p.X < min.X)) then
             min <- p
@@ -17,11 +18,38 @@ let dist (p1,p2) =
     |> sqrt
 
 let cos p1 p2 = 
-    let x = float p2.X
-    let r =
-        pown p2.X 2 + pown (p2.Y - p1.Y) 2
-        |> float
-        |> sqrt
-    x/r
+    match p1 = p2 with
+    | true -> 2.0
+    | false ->
+        let x = float p2.X
+        let r =
+            pown p2.X 2 + pown (p2.Y - p1.Y) 2
+            |> float
+            |> sqrt
+        x/r
 
-let perimeter points = List.sumBy dist (points |> List.pairwise)
+let perimeter (points:Point list) = 
+    let matrix = points @ [List.head points]
+    List.sumBy dist (matrix |> List.pairwise)
+
+let grahamScan points =
+    let stack = Stack<Point>()
+    let min = findMin points
+    let sorted = List.sortByDescending (cos min) points
+    let mutable counter = 0
+    for point in sorted do
+        match counter < 3 with
+        | true -> 
+            counter <- counter + 1
+            stack.Push point
+        | false -> 
+            let mutable top = stack.Pop()
+            let mutable top2 = stack.Pop()
+            while ccw top2 top point <= 0 do
+                top <- top2
+                top2 <- stack.Pop()
+            stack.Push top2
+            stack.Push top
+            stack.Push point
+    let stackList = List.ofSeq stack
+    perimeter stackList
--- Test/Test.fs
+++ Test/Test.fs
@@ -3,8 +3,11 @@ open Code
 
 [<EntryPoint>]
 let main argv =
-    let p1 = {X = 3; Y = 0}
-    let p2 = {X = -1; Y = 0}
-    let cos = cos p1 p2
+    let p1 = {X = 0; Y = 0}
+    let p2 = {X = 1; Y = 0}
+    let p3 = {X = 1; Y = 1}
+    let p4 = {X = 0; Y = 1}
+    let points = [p1; p2; p3; p4]
+    let res = grahamScan points
     0
 
